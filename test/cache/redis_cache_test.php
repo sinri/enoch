872874601b9e780da56b0930c5511e56c9278ed1
Sinri Edogawa <e.joshua.s.e@gmail.com>
2017-07-20 11:06:28 +0800

    test for redis cache more

--- test/cache/redis_cache_test.php
+++ test/cache/redis_cache_test.php
@@ -19,19 +19,8 @@ if (file_exists(__DIR__ . '/redis_config.php')) {
 
 $cache = new \sinri\enoch\service\RedisCache($host, $port, $database, $password);
 
-$cache->saveObject('A', 'Apple', 5);
-$a = $cache->getObject('A');
-echo "[should be Apple]: " . $a . PHP_EOL;
 
-$cache->saveObject('A', "Amazing", 3);
-$a = $cache->getObject('A');
-echo "[should be Amazing]: " . $a . PHP_EOL;
-
-$cache->append('A', ' Grace');
-$a = $cache->getObject('A');
-echo "[should be Amazing Grace]: " . $a . PHP_EOL;
-
-// you cannot set array to redis...
+// 0. you cannot set array to redis...
 //$a = ["name" => "A", "value" => "ALPHA"];
 //$cache->saveObject("A", $a);
 //$a = $cache->getObject("A");
@@ -42,10 +31,19 @@ echo "[should be Amazing Grace]: " . $a . PHP_EOL;
 //$a = $cache->getObject("A");
 //echo "[should be an ordered array]: " . json_encode($a) . PHP_EOL;
 
+// 1. common save
+
+$cache->saveObject('A', 'Apple', 5);
+$a = $cache->getObject('A');
+echo "[should be Apple]: " . $a . PHP_EOL;
+
+// 2. common remove
 
 $cache->removeObject('A');
 $a = $cache->getObject('A');
 echo "[should be FALSE]: " . json_encode($a) . PHP_EOL;
+
+// 3. special for redis INCR/DECR
 
 $cache->increase('A', 1);
 $a = $cache->getObject('A');
@@ -62,6 +60,18 @@ echo "[should be 2]: " . $a . PHP_EOL;
 $cache->increaseFloat('A', 1.5);
 $a = $cache->getObject('A');
 echo "[should be 3.5]: " . $a . PHP_EOL;
+
+// 4. special for redis APPEND
+
+$cache->saveObject('A', "Amazing", 3);
+$a = $cache->getObject('A');
+echo "[should be Amazing]: " . $a . PHP_EOL;
+
+$cache->append('A', ' Grace');
+$a = $cache->getObject('A');
+echo "[should be Amazing Grace]: " . $a . PHP_EOL;
+
+// 5. timeout
 
 sleep(5);
 $a = $cache->getObject('A');
